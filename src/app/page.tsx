fe6104e4cffb170221633d4b1bca147282dcbaee
madreesm334033 <madreesm334033@gmail.com>
2025-12-10 04:34:53 -0800

    bean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,26 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      how are you
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/bean.jfif" alt="" width="1000" height="1100"/>
+  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p className="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption className="font-medium">
+      <div className ="text-sky-500 dark:text-sky-400">
+        MR Bean
+      </div>
+      <div className="text-slate-700 dark:text-slate-500">
+        A joker
+      </div>
+    </figcaption>
+  </div>
+</figure>
     </div>
   )
 }
